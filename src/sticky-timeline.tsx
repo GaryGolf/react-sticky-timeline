5815932f8e5b499100da7b2ede1735fac02e969f
GaryGolf <garygolf@mail.ru>
2018-07-10 16:30:07 +0300

    add styles

---
+++ src/sticky-timeline.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import StickyDate from './sticky-date';
+import StickyList from 'react-sticky-list';
+import * as moment from 'moment';
+import * as styles from './sticky-timeline.css';
+
+export type Moment = moment.Moment;
+export type DateType = string | number | Date | Moment;
+export type DateFormat = 'Date' | 'Moment' | 'number' | 'string';
+
+export interface StickyElement {
+  idx: number;
+  position: string;
+  component: JSX.Element;
+}
+
+export interface StickyPosition {
+  idx: number;
+  position: string;
+}
+
+export interface StickyTimelineProps  {
+  value: DateType;
+  monthCount?: number;
+  type?: DateFormat;
+  onChange: (date:DateType) => void;
+}
+
+// interface State {
+//   elements: StickyElement[];
+//   isTop: boolean;
+//   isBottom: boolean;
+// }
+
+export class StickyTimeline extends React.Component<StickyTimelineProps, null> {
+
+  static defaultProps = { type: 'Date', monthCount: 6 }
+  private container: HTMLDivElement;
+
+  private handleDateClick = (date:Moment) => {
+    this.props.onChange(date.toDate())
+  }
+
+  private renderCalendar = (value: DateType, monthCount: number) => new Array(monthCount)
+    .fill(null)
+    .map((x,i) => moment().add(i, 'month').startOf('month'))
+    .reduce((acc, m) => {
+
+      const activeMonth = moment(value).isSame(m, 'month');
+      const month = (
+        <h4 key={m.format('MMM YYYY')}  
+          className={['sticky-month', activeMonth ? '__active' : ''].join(' ')} >
+          {m.format('MMMM')}
+        </h4>
+      )
+
+      acc.push(month);
+      
+      const dayList = new Array(m.daysInMonth())
+        .fill(null)
+        .map((x, i) => moment(m).add(i, 'days'))
+        .filter(d => moment().isBefore(d,'days'))
+        .map(d => (
+          <StickyDate
+            date={moment(d)}
+            key={moment(d).format('DD MMMM YYYY')}
+            className={[
+              'sticky-date', 
+              moment(value).isSame(d, 'day') ? '__active' : ''
+            ].join(' ')}
+            onClick={this.handleDateClick}
+          />
+        ))
+
+      return acc.concat(dayList);
+    },[]);
+
+  render() {
+    const { value, monthCount } = this.props;
+    const calendar = this.renderCalendar(value, monthCount);
+
+    return (
+      <div className="sticky-timeline" ref={el => this.container = el}>
+        <StickyList>
+          {calendar}
+        </StickyList>
+        <style>{styles.toString()}</style>
+      </div>
+    )
+  }
+} 
+
+export default StickyTimeline;
