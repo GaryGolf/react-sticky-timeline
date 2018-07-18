cd231beca37e53f33feef06930c80a765fdcd1fe
GaryGolf <garygolf@mail.ru>
2018-07-18 18:37:31 +0300

    create demo component

---
+++ example/demo.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface Props {}
+
+export default class Demo extends React.PureComponent<Props> {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
