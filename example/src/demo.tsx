9b76635696c3099c132f1934bf07066c305f878c
GaryGolf <garygolf@mail.ru>
2018-07-22 14:54:11 +0300

    test demo

--- example/src/demo.tsx
+++ example/src/demo.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import StickyList from 'react-sticky-list';
 import * as styles from './demo.css';
 interface Props {}
 
@@ -6,7 +7,78 @@ export default class Demo extends React.PureComponent<Props, {}> {
   render() {
     return (
       <div className={styles.container}>
-        Hello
+        <StickyList className={styles.list}>
+          <header>11</header>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <header>12</header>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <header>13</header>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <header>14</header>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <header>15</header>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+          <div>asfgfgaasfgasfg</div>
+        </StickyList>
       </div>
     )
   }
